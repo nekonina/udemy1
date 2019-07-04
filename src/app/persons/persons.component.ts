74b96625e46489ef7e71162a5f5e3170d7ef238e
Sergio Luis Medina Hernández <sermed19@gmail.com>
2019-07-04 15:11:37 -0400

    listo repaso/aprendizaje angular del curso /0/

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { PersonsService } from "./persons.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-persons",
   templateUrl: "./persons.component.html",
   styleUrls: ["./persons.component.scss"]
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personsList: string[];
-
+  isFetching = false;
+  private personsListSubs: Subscription;
   constructor(private prsService: PersonsService) {}
 
   ngOnInit() {
-    this.personsList = this.prsService.persons;
+    this.prsService.fetchPersons();
+    this.isFetching = true;
+    this.personsListSubs = this.prsService.personsChanged.subscribe(persons => {
+      this.isFetching = false;
+      this.personsList = persons;
+    });
   }
+
+  onRemovePerson(personName: string) {
+    this.prsService.removePerson(personName);
+  }
+  ngOnDestroy(){
+    this.personsListSubs.unsubscribe();
+  };
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,13 +1,40 @@
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class PersonsService {
-  persons: string[] = ["Max", "Iri", "Fer", "Manu"];
+  personsChanged = new Subject<string[]>();
+  persons: string[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  fetchPersons() {
+    this.http
+      .get<any>("https://swapi.co/api/people")
+      .pipe(
+        map(resData => {
+          return resData.results.map(character => character.name);
+        })
+      )
+      .subscribe(transformData => {
+        this.personsChanged.next(transformData);
+      });
+  }
 
   addPerson(name: string) {
     this.persons.push(name);
+    this.personsChanged.next(this.persons);
+  }
+
+  removePerson(name: string) {
+    this.persons = this.persons.filter(person => {
+      return person !== name;
+    });
     console.log(this.persons);
+    this.personsChanged.next(this.persons);
   }
 }
